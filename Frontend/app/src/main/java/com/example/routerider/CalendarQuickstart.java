2b537ce88b3591bb8a15448c9c6382e498f0de28
koltonsky <koltonluu@gmail.com>
2023-10-21 21:40:37 -0700

    Started adding google calender api, but getting error to create token dir

--- Frontend/app/src/main/java/com/example/routerider/CalendarQuickstart.java
+++ Frontend/app/src/main/java/com/example/routerider/CalendarQuickstart.java
@@ -1,27 +1,14 @@
-package com.example.routerider.fragments;
+package com.example.routerider;
 
-import static com.example.routerider.CalendarQuickstart.APPLICATION_NAME;
-import static com.example.routerider.CalendarQuickstart.JSON_FACTORY;
-import static com.example.routerider.CalendarQuickstart.getCredentials;
+import android.os.Environment;
+import android.util.Log;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.example.routerider.CalendarQuickstart;
-import com.example.routerider.R;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
@@ -32,6 +19,7 @@ import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,8 +28,11 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
-public class ScheduleFragment extends Fragment {
-
+/* class to demonstrate use of Calendar events list API */
+public class CalendarQuickstart {
+    /**
+     * Application name.
+     */
     public static final String APPLICATION_NAME = "RouteRider";
     /**
      * Global instance of the JSON factory.
@@ -105,65 +96,34 @@ public class ScheduleFragment extends Fragment {
         return credential;
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
-        Button getCalendar = view.findViewById(R.id.connectCalendar);
-
-        getCalendar.setOnClickListener(v -> {
-            final NetHttpTransport HTTP_TRANSPORT;
-            try {
-                HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            } catch (GeneralSecurityException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            Calendar service =
-                    null;
-            try {
-                service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service =
+                new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                         .setApplicationName(APPLICATION_NAME)
                         .build();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
 
-            // List the next 10 events from the primary calendar.
-            DateTime now = new DateTime(System.currentTimeMillis());
-            Events events = null;
-            try {
-                events = service.events().list("primary")
-                        .setMaxResults(10)
-                        .setTimeMin(now)
-                        .setOrderBy("startTime")
-                        .setSingleEvents(true)
-                        .execute();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            List<Event> items = events.getItems();
-            if (items.isEmpty()) {
-                System.out.println("No upcoming events found.");
-            } else {
-                System.out.println("Upcoming events");
-                for (Event event : items) {
-                    DateTime start = event.getStart().getDateTime();
-                    if (start == null) {
-                        start = event.getStart().getDate();
-                    }
-                    System.out.printf("%s (%s)\n", event.getSummary(), start);
+        // List the next 10 events from the primary calendar.
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+        List<Event> items = events.getItems();
+        if (items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        } else {
+            System.out.println("Upcoming events");
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    start = event.getStart().getDate();
                 }
+                System.out.printf("%s (%s)\n", event.getSummary(), start);
             }
-        });
-
-
-
-
-        return view;
+        }
     }
-
 }

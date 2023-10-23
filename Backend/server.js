9eb6dc116a3ef50260737a1f5a80f77f7745bc0c
leonguo736 <leonguo736@gmail.com>
2023-10-23 12:31:08 -0700

    initReminders partially complete, can get a list of nearby buses when provided an address. need to loop through buses, get leave time for each and choose best one.

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const https = require('https')
-const path = require('path')
-const fs = require('fs')
-const port = 8081
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const queryString = require('querystring');
+const port = 8081;
 const { MongoClient} = require('mongodb');
+// const ApiKeyManager = require('@esri/arcgis-rest-request');
+const {ApiKeyManager} = require('@esri/arcgis-rest-request');
+const {geocode} = require('@esri/arcgis-rest-geocoding');
 
 const app = express()
 app.use(express.json());
@@ -70,6 +74,107 @@ const sslServer = https.createServer({
     cert:fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem'))
 }, app)
 
+/**
+ * Function outputs an array of 7 entries, with each entry corresponding to when the user should be reminded to leave for class on that day.
+ * Instead of array, could also directly modify the schedule.
+ * Translink Open API Key: crj9j8Kj97pbPkkc61dX
+ * Geocoding API key: AAPK3c726265cc41485bb57c5512e98cf912OLoJQtidjOlcqjdpa0Pl773UqNoOYfwApr6ORYd8Lina8_K0sEbdcyXsNfHFqLKE if error 498 invalid token, create new key
+ * 
+ * Function takes in an home/starting address and a schedule (includes location/address of classes eventually, but temporarily assume UBC for simplicity) 
+ * @param {*} schedule 
+ * @param {*} address 
+ */
+async function initReminders(schedule, address) {
+    /* Get static schedule of closest bus stop to home address */
+    var buses = [];
+    // get closest bus stop to home address
+    console.log("calling translink api");
+    buses = await getNearestBuses(address);
+    console.log("initReminders(): returned buses: " + buses[1].StopNo);
+}
+initReminders("xxx", "xxx");
+
+function getLatLong(address) {
+  return new Promise((resolve, reject) => {
+    var lat = 0; // default value
+    var long = 0; // default value
+    var coords = [lat, long];
+  
+    var apiKey_geo = "AAPK3c726265cc41485bb57c5512e98cf912OLoJQtidjOlcqjdpa0Pl773UqNoOYfwApr6ORYd8Lina8_K0sEbdcyXsNfHFqLKE";
+    var authentication_geo = ApiKeyManager.fromKey(apiKey_geo);
+  
+    console.log("getLatLong: calling geocode api");
+    geocode({
+      address: address,
+      authentication: authentication_geo
+    
+    }).then(response => {
+      // x: longitude, y: latitude
+      console.log(response.candidates[0].location); // => { x: -77.036533, y: 38.898719, spatialReference: ... }
+      lat = response.candidates[0].location.y;
+      long = response.candidates[0].location.x;
+      console.log("helper function getLatLong() output: " + coords + " " + lat + " " + long);
+      coords[0] = lat.toFixed(5);
+      coords[1] = long.toFixed(5);
+      resolve(coords);
+    }).catch(error => {
+      console.log(error);
+    });
+  });
+}
+
+async function getNearestBuses(address) {
+  return new Promise((resolve, reject) => {
+    lat = 0;
+    long = 0;
+  
+    console.log("--------------------");
+    console.log("getBuses: calling getlatlong");
+    var address = "7746 Berkley Street, Burnaby, British Columbia, Canada";
+    getLatLong(address).then((coords) => {
+      console.log("initReminders(): returned coords: " + coords);
+      console.log("--------------------");
+      lat = coords[0];
+      long = coords[1];
+    
+      var apiKey = "crj9j8Kj97pbPkkc61dX";
+    
+      const queryParams = queryString.stringify({
+        lat: lat,
+        long: long,
+        radius: 500, // Set the radius to your desired value (in meters)
+      });
+    
+      const options = {
+        hostname: 'api.translink.ca',
+        path: `/rttiapi/v1/stops?apikey=${apiKey}&${queryParams}`,
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+        },
+      };
+      console.log("getnearestbuses(): options set. " + options.path);
+    
+      const request = https.get(options, (response) => {
+        let data = '';
+        console.log("getnearestbuses(): request sent");
+    
+        response.on('data', (chunk) => {
+          data += chunk;
+        });
+    
+        response.on('end', () => {
+          console.log("getNearestBuses() return");
+          console.log(JSON.parse(data));
+          console.log("--------------------");
+          resolve(JSON.parse(data));
+        });
+      }).on("error", (err) => {
+          console.log("Error: " + err.message);
+      });
+    });
+  });
+}
 
 connectToDatabase();
 sslServer.listen(port, () => console.log('Secure server :) on port ' + port))

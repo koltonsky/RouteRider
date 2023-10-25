835cf081f65dc8b3e65e18799e3af160702091ba
koltonsky <koltonluu@gmail.com>
2023-10-25 13:28:57 -0700

    Grabbed time and location for events

--- Frontend/app/src/main/java/com/example/routerider/ScheduleItem.java
+++ Frontend/app/src/main/java/com/example/routerider/ScheduleItem.java
@@ -3,6 +3,13 @@ package com.example.routerider;
 public class ScheduleItem {
     private String title;
     private String location;
-    private String date;
-    private String time;
+    private String startTime;
+    private String endTime;
+
+    public ScheduleItem(String eventSummary, String eventLocation, String startTimeString, String endTimeString) {
+        title = eventSummary;
+        location = eventLocation;
+        startTime = startTimeString;
+        endTime = endTimeString;
+    }
 }
--- Frontend/app/src/main/java/com/example/routerider/fragments/ScheduleFragment.java
+++ Frontend/app/src/main/java/com/example/routerider/fragments/ScheduleFragment.java
@@ -11,18 +11,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.routerider.R;
+import com.example.routerider.ScheduleItem;
 import com.example.routerider.User;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.CalendarList;
 import com.google.api.services.calendar.model.CalendarListEntry;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -62,6 +67,8 @@ class CalendarAsyncTask extends AsyncTask<Calendar, Void, Void> {
     @Override
     protected Void doInBackground(Calendar... calendars) {
         Calendar service = calendars[0];
+        List<ScheduleItem> eventList = new ArrayList<>();
+
         try {
             CalendarList calendarList = service.calendarList().list().execute();
             List<CalendarListEntry> items = calendarList.getItems();
@@ -78,6 +85,40 @@ class CalendarAsyncTask extends AsyncTask<Calendar, Void, Void> {
                     System.out.println("Calendar ID: " + calendarId);
                     System.out.println("Summary: " + summary);
                     System.out.println();
+
+                    // List events for the calendar
+                    Events events = service.events().list(calendarId)
+                            .setTimeMin(new DateTime(System.currentTimeMillis()))
+                            .setTimeMax(new DateTime(System.currentTimeMillis() + 86400000)) // Set the time range as needed
+                            .execute();
+
+                    List<Event> itemsEvents = events.getItems();
+                    for (Event event : itemsEvents) {
+                        String eventId = event.getId();
+                        String eventSummary = event.getSummary();
+                        String eventLocation = event.getLocation();
+                        DateTime startTime = event.getStart().getDateTime();
+                        DateTime endTime = event.getEnd().getDateTime();
+
+                        // Format the start and end times as strings (you can customize the format)
+                        String startTimeString = (startTime != null) ? startTime.toString() : "N/A";
+                        String endTimeString = (endTime != null) ? endTime.toString() : "N/A";
+
+                        System.out.println("Event ID: " + eventId);
+                        System.out.println("Event Summary: " + eventSummary);
+                        System.out.println("Event Location: " + eventLocation);
+                        System.out.println("Start Time: " + startTimeString);
+                        System.out.println("End Time: " + endTimeString);
+                        System.out.println();
+
+                        ScheduleItem newEvent = new ScheduleItem(
+                                eventSummary,
+                                eventLocation,
+                                startTimeString,
+                                endTimeString);
+
+                        eventList.add(newEvent);
+                    }
                 }
             }
         } catch (IOException e) {
@@ -85,8 +126,11 @@ class CalendarAsyncTask extends AsyncTask<Calendar, Void, Void> {
             // Handle the network-related exception
             // You can also use a handler to update the UI with any results or error messages
         }
+
+        //return eventList;
         return null;
     }
+
 
     @Override
     protected void onPostExecute(Void result) {

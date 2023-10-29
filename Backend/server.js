09db7f7f6b723bc940f6d390e5e18738385822da
leonguo736 <leonguo736@gmail.com>
2023-10-29 01:49:31 -0700

    can pull schedule from db and create route sucessfully!!

--- Backend/server.js
+++ Backend/server.js
@@ -17,6 +17,7 @@ const axios = require('axios')
 const user = require('./routes/user.js')
 const schedule = require('./routes/schedule.js');
 const { get } = require('http');
+const { time } = require('console');
 
 // MongoDB connection setup
 const uri = 'mongodb://0.0.0.0:27017'; // Replace with your MongoDB connection string
@@ -166,39 +167,34 @@ async function initReminders(userEmail, date) {
   // buses = await getNearestBuses(address);
   // console.log("initReminders(): returned buses: " + buses[1].StopNo + " " + buses.length);
 
-  // var schedule = db.collections['schedulelist'].find({email: userEmail}); // ask gina
-  // var schedule = await getSchedule("johndoe@example.com");
+  // client.db('ScheduleDB').collection('schedulelist').findOne({id: "johndoe@example.com"}).then((result) => {
+  //   console.log("retrieved schedule: " + result.schedule);
+  // });
+  var schedule = await client.db('ScheduleDB').collection('schedulelist').findOne({id: userEmail});
   console.log("initReminders(): returned schedule: " + schedule);
-  var trip = {};
-  // for (var i = 0; i < schedule.length; i++) {
-  //   if (schedule[i].startTime.split('T')[0] == date) { // assumes start time is a string in ISO format (e.g. "2023-09-06T15:00:00.000-07:00")
-  //     trip = planTransitTrip(address, "UBC Exchange Bus Loop", schedule[i].startTime);
-  //     break;
-  //   }
-  // }
-  planTransitTrip('5870 Rumble Street, Burnaby, BC', "UBC Exchange Bus Loop", new Date("2023-10-30T18:00:00.000Z")).then((trip) => {
+  console.log(schedule.schedule.events[0].eventName);
+
+  var timeOfFirstEvent = "";
+  for (var i = 0; i < schedule.schedule.events.length; i++) {
+    console.log("initReminders(): " + schedule.schedule.events[i].date + " " + date);
+    if (schedule.schedule.events[i].date == date) { 
+      console.log("initReminders(): startTime " + schedule.schedule.events[i].timeFrame.start);
+      timeOfFirstEvent = combineDateAndTime(schedule.schedule.events[i].date, schedule.schedule.events[i].timeFrame.start);
+      break;
+    }
+  }
+  console.log("initReminders(): returned startTime: " + timeOfFirstEvent);
+
+  planTransitTrip('5870 Rumble Street, Burnaby, BC', "UBC Exchange Bus Loop", new Date(timeOfFirstEvent)).then((trip) => {
     console.log("initReminders(): returned trip: " + trip + " " + trip.routes[0].legs[0].steps[0].travel_mode);
     // fields for object to be returned to frontend
     var id = '';
     var leaveTime = '';
     var type = '';
     var more = {};
-    // transit_details-->line-->vehicle--> if name == "Subway", type = "SkyTrain", else if name == "Bus", type = "Bus", else type = "Walk"-->if skytrain, name == line.name, if Bus, name == line.short_name
-      // Access route information such as summary, distance, duration, steps, etc.
-    //   console.log(`Route ${index + 1}:`);
-    //   console.log(`Summary: ${route.summary}`);
-    //   console.log(`Distance: ${route.legs[0].distance.text}`);
-    //   console.log(`Duration: ${route.legs[0].duration.text}`);
-    //   console.log('Steps:');
-    //   route.legs[0].steps.forEach((step, stepIndex) => {
-    //     console.log(`Step ${stepIndex + 1}: ${step.html_instructions}`);
-    //   });
-    //   console.log('-----------------------');
-    // var route = trip.routes[0].legs[0].steps[0].travel_mode;
     
     var curStep = {}; // maybe properly define this object later
     var returnList = [];
-    var firstEventTime = "default";
     more.distance = trip.routes[0].legs[0].distance.text;
     more.duration = trip.routes[0].legs[0].duration.text;
     more.arrival_time = trip.routes[0].legs[0].arrival_time.text;
@@ -246,10 +242,11 @@ async function initReminders(userEmail, date) {
     for (var i = 0; i < returnList.length - 1; i++) {
       if (i == 0 && returnList[i]._type == "Walk") { 
         returnList[i]._leaveTime = returnList[returnList.length - 1].departure_time;
-        returnList[i]._leaveTimeNum = timestampToTime(returnList[returnList.length - 1].departure_time);
+        returnList[i]._leaveTimeNum = timeToTimestamp(returnList[returnList.length - 1].departure_time);
       }
       else if ((i == returnList.length - 2) && returnList[i]._type == "Walk") { 
-        returnList[i]._leaveTime = timeToTimestamp(returnList[returnList.length - 1].arrival_time);
+        returnList[i]._leaveTime = returnList[returnList.length - 1].arrival_time;
+        returnList[i]._leaveTimeNum = timeToTimestamp(returnList[returnList.length - 2].arrival_time);
       }
       else if (returnList[i]._type == "Walk") { // assumes 'type' for next array entry is either "Bus" or "SkyTrain"
         returnList[i]._leaveTime = timestampToTime(returnList[i + 1]._leaveTimeNum - returnList[i]._leaveTimeNum);
@@ -264,13 +261,45 @@ async function initReminders(userEmail, date) {
 // getSchedule("johnjoe@example.com", "POST").then((my_schedule) => {
 //   console.log("returned schedule: " + my_schedule);
 // });
-// client.db('ScheduleDB').collection('schedulelist').insertOne({id: 'test_id', schedule: 'test schedule'}).then((result) => {
+var dummy_schedule = {
+  "email": "johndoe@email.com",
+  "events": [
+    {
+      "eventName": "Meeting with Client",
+      "date": "2023-10-23",
+      "timeFrame": {
+        "start": "08:00 AM",
+        "end": "10:00 AM"
+      },
+      "address": "123 Main St, City, Country",
+      "geolocation": {
+        "latitude": 123.456789,
+        "longitude": 987.654321
+      }
+    },
+    {
+      "eventName": "Lunch with Colleague",
+      "date": "2023-10-24",
+      "timeFrame": {
+        "start": "02:30 PM",
+        "end": "04:30 PM"
+      },
+      "address": "456 Elm St, City, Country",
+      "geolocation": {
+        "latitude": 45.678901,
+        "longitude": -67.890123
+      }
+    }
+  ]
+};
+// client.db('ScheduleDB').collection('schedulelist').insertOne({id: 'dummy@gmail.com', schedule: dummy_schedule}).then((result) => {
 //   console.log("inserted schedule");
 // });
 // client.db('ScheduleDB').collection('schedulelist').findOne({id: "johndoe@example.com"}).then((result) => {
 //   console.log("retrieved schedule: " + result.schedule);
 // });
-var dummy = initReminders("xxx", "xxx");
+
+var dummy = initReminders("dummy@gmail.com", "2023-10-30");
 // planTransitTrip('5870 Rumble Street, Burnaby, BC', "UBC Exchange Bus Loop", new Date("2023-10-28T18:00:00.000Z"));
 // getNearestBuses("EB Rumble St @ Gilley Ave").then((buses) => {
 //   console.log(buses[0].StopNo + " " + buses.length);
@@ -459,4 +488,28 @@ function timestampToTime(timestamp) {
   return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
 }
 
+function combineDateAndTime(dateString, timeString) {
+  // Convert the time to 24-hour format and add seconds
+  const timeComponents = timeString.match(/(\d+):(\d+) (A|P)M/);
+  if (!timeComponents) {
+      throw new Error("Invalid time format");
+  }
+  
+  const hours = parseInt(timeComponents[1]);
+  const minutes = parseInt(timeComponents[2]);
+  const isPM = timeComponents[3].toLowerCase() === 'pm';
 
+  // Adjust hours for PM
+  if (isPM && hours < 12) {
+      hours += 12;
+  }
+
+  const isoDateTimeString = `${dateString}T${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:00`;
+  const date = new Date(isoDateTimeString);
+
+  if (isNaN(date)) {
+      throw new Error("Invalid date or time format");
+  }
+
+  return date.toISOString();
+}

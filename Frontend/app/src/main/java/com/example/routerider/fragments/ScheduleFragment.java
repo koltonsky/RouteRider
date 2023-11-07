9af7d126010e0d4e91bf46f3888e43aeb94cbafe
Michael Banna <michael.banna@polymtl.ca>
2023-11-06 17:14:01 -0800

    remove unused import

--- Frontend/app/src/main/java/com/example/routerider/fragments/ScheduleFragment.java
+++ Frontend/app/src/main/java/com/example/routerider/fragments/ScheduleFragment.java
@@ -1,7 +1,5 @@
 package com.example.routerider.fragments;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
